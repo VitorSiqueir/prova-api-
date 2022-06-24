87bc67fb63065a04be2484668df068972c696a98
VitorSiquer <siqueirabragavitor@gmail.com>
2022-06-24 15:26:53 -0300

    aaa

---
+++ site/src/vilão/index.js
@@ -0,0 +1,13 @@
+import './index.scss';
+
+export function vilão() {
+  return (
+    <div className="vilão">
+      <header className="header">
+
+      </header>
+    </div>
+  );
+}
+
+export default vilão;
